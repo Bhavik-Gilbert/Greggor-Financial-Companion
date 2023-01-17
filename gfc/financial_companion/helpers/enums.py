92916995966ed40fba756b6c5f3589b8e6539cbe
bhavik gilbert <bhaviklob@gmail.com>
2023-01-17 20:07:56 +0000

    feat: create timespan enum

--- gfc/financial_companion/helpers/enums.py
+++ gfc/financial_companion/helpers/enums.py
@@ -2,9 +2,16 @@ from enum import Enum
 
 class MessageType(Enum):
     """ENUM defining bootstrap message types"""
-    DANGER = "danger"
-    SUCCESS = "success"
-    WARNING = "warning"
-    INFO = "info"
-    PRIMARY = "primary"
-    SECONDARY = "secondary"
+    DANGER: str = "danger"
+    SUCCESS: str = "success"
+    WARNING: str = "warning"
+    INFO: str = "info"
+    PRIMARY: str = "primary"
+    SECONDARY: str = "secondary"
+
+class Timespan(Enum):
+    """ENUM for generic timespans"""
+    DAY: str = "day"
+    WEEK: str = "week"
+    MONTH: str = "month"
+    YEAR: str = "year"
