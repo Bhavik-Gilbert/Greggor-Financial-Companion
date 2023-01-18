fcca821637a8108109cee78c554e51fcc11d0805
Bhavik Glibert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2023-01-18 12:06:03 +0000

    fix: remove Target from admin

--- gfc/financial_companion/admin.py
+++ gfc/financial_companion/admin.py
@@ -1,7 +1,5 @@
 """Configuration of the admin interface of financial_companion."""
 
 from django.contrib import admin
-from .models import Target
 
 # Register your models here.
-admin.site.register(Target)
