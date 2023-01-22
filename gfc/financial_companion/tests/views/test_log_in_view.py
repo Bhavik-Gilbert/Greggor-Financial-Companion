d5f795e3112879db3da2d1c6ac92bb5a16f6d8c3
Matthew Palmer <matthewpalmer135@gmail.com>
2023-01-22 15:11:32 +0000

    test: Implement log in view tests

---
+++ gfc/financial_companion/tests/views/test_log_in_view.py
@@ -0,0 +1,59 @@
+from django.contrib.auth.hashers import check_password
+from django.urls import reverse
+
+from .test_view_base import ViewTestCase
+from ..helpers.log_in_helpers import LogInTester
+from financial_companion.forms import UserLogInForm
+from financial_companion.models import User
+
+class LogInViewTestCase(ViewTestCase, LogInTester):
+    """Unit tests of the log in view"""
+
+    def setUp(self):
+        self.url = reverse('log_in')
+        self.user = User.objects.get(username='@johndoe')
+
+    def test_log_in_url(self):
+        self.assertEqual(self.url,'/log_in/')
+
+    def test_get_log_in(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, UserLogInForm))
+        self.assertFalse(form.is_bound)
+
+    def test_successful_log_in(self):
+        form_input = {'username': '@johndoe', 'password': 'Password123'}
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertTrue(self.is_logged_in())
+        response_url = reverse('dashboard')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'pages/dashboard.html')
+
+    def test_unsuccessful_log_in(self):
+        form_input = {'username': '@johndoe', 'password': 'WrongPassword123'}
+        response = self.client.post(self.url, form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, UserLogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self.is_logged_in())
+
+    def test_invalid_form_for_log_in(self):
+        wrong_form_input = {'username': 'johndoe', 'password': 'WrongPassword123'}
+        response = self.client.post(self.url, wrong_form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, UserLogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self.is_logged_in())
+
+    # def test_user_redirects_when_not_logged_in(self):
+    #     dashboard = reverse('dashboard')
+    #     response = self.client.get(dashboard)
+    #     response_url = reverse('log_in')
+    #     self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
