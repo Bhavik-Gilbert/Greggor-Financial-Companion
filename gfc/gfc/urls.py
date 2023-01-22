c29c89f2cb57a98ba0912c43bfb1a6bb36bbe96d
Bhavik Glibert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2023-01-22 13:12:57 +0000

    fix: add comma after dashboard url pattern

--- gfc/gfc/urls.py
+++ gfc/gfc/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view),
     path('sign_up/', views.sign_up_view, name='sign_up'),
-    path('dashboard/', views.dashboard_view, name='dashboard')
+    path('dashboard/', views.dashboard_view, name='dashboard'),
     path('add_monetary_account', views.add_monetary_account_view, name="add_monetary_account"),
     re_path(
         'edit_monetary_account/(?P<account_type>\w+)/(?P<pk>\d+)/$', 
