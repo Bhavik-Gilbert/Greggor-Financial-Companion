7d814a60543ff9a58d0a593a262b600f6fd10bc1
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2023-01-24 17:16:19 +0000

    feat: update category list view to filter by user

--- gfc/financial_companion/views/category_views.py
+++ gfc/financial_companion/views/category_views.py
@@ -1,10 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from ..models import Category
-        
+from django.contrib.auth.decorators import login_required
+
+@login_required     
 def category_list_view(request: HttpRequest) -> HttpResponse:
     """View to view list of all existing categories"""
 
-    categories = Category.objects.all
+    categories = Category.objects.filter(user_id = request.user)
     return render(request, "pages/category_list.html", {"categories": categories})
 
