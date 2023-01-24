cb115e2449aa9a32e611e04a2812a7d96e83a6ae
Matthew Palmer <matthewpalmer135@gmail.com>
2023-01-24 16:48:25 +0000

    test: Implement tests for view_user_pot_accounts view

--- gfc/financial_companion/tests/views/test_view_base.py
+++ gfc/financial_companion/tests/views/test_view_base.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 from ..test_base import BaseTestCase
 from ...models import User
+from django.urls import reverse
 
 class ViewTestCase(BaseTestCase):
     """
@@ -19,7 +20,7 @@ class ViewTestCase(BaseTestCase):
         Return True if the user was logged in successfully.
         Otherwise, return False.
         """
-        
+
         return self.client.login(username=user.username, password=password)
 
     def _is_logged_in(self) -> bool:
@@ -48,7 +49,7 @@ class ViewTestCase(BaseTestCase):
                 url += key + "=" + kwargs[key]
 
         return url
-    
+
     def _assert_require_login(self, url: str) -> None:
         """Asserts users are not allowed to access this page when logged out"""
         user: User = auth.get_user(self.client)
@@ -57,7 +58,7 @@ class ViewTestCase(BaseTestCase):
         expected_url: str = self._reverse_with_query("log_in", next=url)
         self.assertRedirects(response, expected_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response,  "pages/log_in.html")
-    
+
     def _assert_require_logout(self, url: str) -> None:
         """Asserts users are not allowed to access this page when logged in"""
         user: User = auth.get_user(self.client)
@@ -66,3 +67,8 @@ class ViewTestCase(BaseTestCase):
         redirect_url: str = reverse('dashboard')
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response,  "pages/dashboard.html")
+
+    def reverse_with_next(self, url_name, next_url):
+        url = reverse(url_name)
+        url += f"?next={next_url}"
+        return url
---
+++ gfc/financial_companion/tests/views/test_view_user_pot_accounts_view.py
@@ -0,0 +1,30 @@
+from django.urls import reverse
+from .test_view_base import ViewTestCase
+from financial_companion.models import User, PotAccount
+
+class ViewAccountsViewTestCase(ViewTestCase):
+    """Tests of the user view pot accounts view."""
+
+    def setUp(self):
+        self.url = reverse('view_accounts')
+        self.user = User.objects.get(username='@johndoe')
+        self.account = PotAccount.objects.get(name='ghi')
+
+    def test_view_accounts_url(self):
+        self.assertEqual(self.url,'/view_accounts/')
+
+    def test_get_view_accounts(self):
+        self.client.login(username=self.user.username, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/view_accounts.html')
+
+    def test_get_view_accounts_redirects_when_not_logged_in(self):
+        redirect_url = self.reverse_with_next('log_in', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_view_accounts_contains_accounts_by_self(self):
+        self.client.login(username=self.user.username, password='Password123')
+        response = self.client.get(self.url)
+        self.assertContains(response, self.account.id)
