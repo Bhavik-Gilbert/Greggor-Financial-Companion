c53135dd26b78fa7eb80b5c832c6aa3a3897a7d6
bhavik gilbert <bhaviklob@gmail.com>
2023-01-25 17:31:53 +0000

    test: tests for home view

--- gfc/financial_companion/tests/views/test_edit_monetary_account_view.py
+++ gfc/financial_companion/tests/views/test_edit_monetary_account_view.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.urls import reverse
 from typing import Any
 
@@ -160,7 +160,7 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         response: HttpResponse = self.client.get(self.bank_url, follow=True)
         self._assert_require_login(self.bank_url)
     
-    def test_invalid_post_pot_page_logeged_in_user_must_be_account_holder(self):
+    def test_invalid_post_pot_page_logged_in_user_must_be_account_holder(self):
         self._login(self.bank_user)
         self.assertNotEqual(self.pot_account.user_id.id, self.bank_user.id)
         form_input: dict[str, Any]= {
@@ -174,7 +174,7 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, "pages/dashboard.html")
     
-    def test_invalid_get_pot_page_logeged_in_user_must_be_account_holder(self):
+    def test_invalid_get_pot_page_logged_in_user_must_be_account_holder(self):
         self._login(self.bank_user)
         self.assertNotEqual(self.pot_account.user_id.id, self.bank_user.id)
         response: HttpResponse = self.client.post(self.pot_url, follow=True)
@@ -183,7 +183,7 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertTemplateUsed(response, "pages/dashboard.html")
 
     
-    def test_invalid_post_bank_page_logeged_in_user_must_be_account_holder(self):
+    def test_invalid_post_bank_page_logged_in_user_must_be_account_holder(self):
         self._login(self.pot_user)
         self.assertNotEqual(self.bank_account.user_id.id, self.pot_user.id)
         form_input: dict[str, Any] = {
@@ -202,7 +202,7 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, "pages/dashboard.html")
     
-    def test_invalid_get_bank_page_logeged_in_user_must_be_account_holder(self):
+    def test_invalid_get_bank_page_loggeed_in_user_must_be_account_holder(self):
         self._login(self.pot_user)
         self.assertNotEqual(self.bank_account.user_id.id, self.pot_user.id)
         response: HttpResponse = self.client.post(self.bank_url, follow=True)
