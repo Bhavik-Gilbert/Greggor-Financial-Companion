31a6d16f14f8da8606a661cfe83fd04e4ac9d453
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2023-01-28 20:08:43 +0000

    feat: update search bar in category list view

--- gfc/financial_companion/views/__init__.py
+++ gfc/financial_companion/views/__init__.py
@@ -4,7 +4,7 @@ from .log_in import log_in_view
 from .log_in import log_out_view
 from .dashboard import dashboard_view
 from .monetary_account_view import add_monetary_account_view, edit_monetary_account_view
-from .category_views import category_list_view
+from .category_views import category_list_view, filter_categories_request
 from .view_accounts import view_user_pot_accounts
 from .create_category import create_category_view
 from .display_transactions import view_users_transactions, filter_transaction_request
--- gfc/financial_companion/views/category_views.py
+++ gfc/financial_companion/views/category_views.py
@@ -1,12 +1,25 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..models import Category
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 
 @login_required     
-def category_list_view(request: HttpRequest) -> HttpResponse:
+def category_list_view(request: HttpRequest, filter_type: str) -> HttpResponse:
     """View to view list of all existing categories"""
 
-    categories = Category.objects.filter(user_id = request.user)
+    if(filter_type == 'all'):
+        categories = Category.objects.filter(user_id = request.user)
+    else:
+        categories = Category.objects.filter(user_id = request.user ).filter(name = filter_type)
+
     return render(request, "pages/category_list.html", {"categories": categories})
 
+@login_required
+def filter_categories_request(request: HttpRequest):
+    categories = Category.objects.filter(user_id = request.user).filter()
+
+    print(request.POST)
+
+    return redirect(reverse('categories_list', kwargs={'filter_type': "all"}))
