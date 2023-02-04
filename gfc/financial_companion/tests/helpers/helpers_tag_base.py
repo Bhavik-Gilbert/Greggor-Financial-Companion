9ba8d623d0451dc635e1ab8bcbefe6c7527bd96d
bhavik gilbert <bhaviklob@gmail.com>
2023-02-04 11:58:28 +0000

    build: add freezegun requirements

---
+++ gfc/financial_companion/tests/helpers/helpers_tag_base.py
@@ -0,0 +1,8 @@
+from ..test_base import BaseTestCase
+
+class HelpersTestCase(BaseTestCase):
+    """
+    Base class for testing helpers.
+    Call super().setUp() in the
+    setUp() method of the subclass.
+    """
