fd474f66e5139d7cff2bf983fcc5f5ccc706d268
Reshma <reshma.dhillon@kcl.ac.uk>
2023-02-06 00:49:27 +0000

    test: for the delete transaction view

---
+++ gfc/financial_companion/tests/views/test_delete_transaction_view.py
@@ -0,0 +1,36 @@
+from .test_view_base import ViewTestCase
+from financial_companion.forms import AddTransactionForm
+from financial_companion.models import Transaction, User
+from django.urls import reverse
+
+class DeleteTransactionViewTestCase(ViewTestCase):
+    """Unit tests of the delete transaction view"""
+
+    def setUp(self):
+        self.url = reverse('delete_transaction', kwargs={"pk": 2})
+        self.user = User.objects.get(username='@johndoe')
+
+    def test_delete_transaction_url(self):
+        self.assertEqual(self.url,'/delete_transaction/2')
+
+    def test_get_delete_transaction(self):
+        self._login(self.user)
+        transaction = Transaction.objects.get(id=2)
+        before_count = User.objects.count()
+        print(transaction.title)
+        response = self.client.get(self.url)
+        print(transaction.title)
+        after_count = User.objects.count()
+        self.assertEqual(before_count-1,after_count)
+        response_url = reverse('dashboard')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+
+    def test_transaction_does_not_exist(self):
+        self._login(self.user)
+        self.url = reverse('delete_transaction', kwargs={'pk': 1000})
+        before_count = User.objects.count()
+        response = self.client.get(self.url)
+        after_count = User.objects.count()
+        self.assertEqual(before_count,after_count)
+        response_url = reverse('dashboard')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
