92c5bbcd33ceb4d7e8de372293bfded9c3cd7240
Matthew Palmer <matthewpalmer135@gmail.com>
2023-02-11 18:45:50 +0000

    feat: Implement tests for User Group Model

--- gfc/financial_companion/models/user_group_model.py
+++ gfc/financial_companion/models/user_group_model.py
@@ -3,8 +3,10 @@ from financial_companion.models import User
 import os
 from financial_companion.helpers import random_filename
 
+
 def change_filename(instance, filename):
     return os.path.join('user_profile', random_filename(filename))
+
 
 class UserGroup(models.Model):
     """Group model used for different groups"""
@@ -12,7 +14,8 @@ class UserGroup(models.Model):
     description: models.CharField = models.CharField(
         max_length=500, blank=False)
     owner_email: models.EmailField = models.EmailField(blank=False)
-    invite_code: models.CharField = models.CharField(max_length=8, blank=False, unique=True)
+    invite_code: models.CharField = models.CharField(
+        max_length=8, blank=False, unique=True)
     members: models.ManyToManyField = models.ManyToManyField(User)
     group_picture: models.ImageField = models.ImageField(
         upload_to=change_filename,
@@ -23,13 +26,13 @@ class UserGroup(models.Model):
 
     def add_member(self, user):
         self.members.add(user)
-    
+
     def remove_member(self, user):
         self.members.remove(user)
 
     def is_member(self, user):
         """Returns whether user is in the group"""
         return user in self.members.all()
-    
+
     def members_count(self):
         return self.members.count()
