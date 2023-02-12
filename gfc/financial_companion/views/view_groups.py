cac046e1278a5d1d91f0f01e159e435a38d4d5b0
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2023-02-12 15:29:30 +0000

    feat: users can now search for group feat: if the user has groups then the view all button will be visible

--- gfc/financial_companion/views/view_groups.py
+++ gfc/financial_companion/views/view_groups.py
@@ -1,9 +1,40 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-# from ..models import Group
+from ..models import UserGroup
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 @login_required
-def all_groups_view(request: HttpRequest) -> HttpResponse:
-    return render(request, "pages/all_groups.html", {"groups": None})
+def all_groups_view(request: HttpRequest, search_name: str) -> HttpResponse:
+
+    userGroups = []
+    user = request.user
+    userEmail = user.email
+    allGroups = UserGroup.objects.all()
+
+    # need to get all the groups the user is in
+    for group in allGroups:
+        if group.owner_email == userEmail:
+            userGroups += group
+        elif group.is_member(user):
+            userGroups += group
+
+    # handling the search
+    if request.method == "POST" and "search" in request.POST:
+        if request.POST["search"].strip(
+        ) == "" or request.POST["search"] is None:
+            return redirect("all_groups_redirect")
+        else:
+            return redirect("all_groups",
+                            search_name=(request.POST["search"]))
+
+    if (search_name != "all"):
+        userGroups.filter(name__icontains=search_name)
+
+    return render(request, "pages/all_groups.html", {"groups": userGroups})
+
+@login_required
+def all_groups_redirect(request: HttpRequest) -> HttpResponse:
+    """Redirect to view list of all existing categories"""
+
+    return redirect("all_groups", search_name="all")
