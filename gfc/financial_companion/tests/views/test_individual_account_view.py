c30e5984df76a8aa72f48a26a3b754f3efa641d8
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2023-02-16 15:55:01 +0000

    test: add tests into individuaa_account view for the targets

--- gfc/financial_companion/tests/views/test_individual_account_view.py
+++ gfc/financial_companion/tests/views/test_individual_account_view.py
@@ -1,9 +1,9 @@
 from .test_view_base import ViewTestCase
-from financial_companion.models import User, Account, PotAccount, BankAccount
+from financial_companion.models import User, Account, PotAccount, BankAccount, AccountTarget
 from django.http import HttpResponse
 from django.urls import reverse
 from django.db.models import Q
-
+from financial_companion.templatetags import get_completeness
 
 class IndividualAccountViewTestCase(ViewTestCase):
     """Unit tests of the individual account view"""
@@ -11,7 +11,7 @@ class IndividualAccountViewTestCase(ViewTestCase):
     def setUp(self):
         self.user: User = User.objects.get(username="@johndoe")
         self.account: PotAccount = PotAccount.objects.get_subclass(
-            user=self.user, id=5)
+            user=self.user, id=3)
         self.url: str = reverse(
             "individual_account",
             kwargs={
@@ -54,6 +54,36 @@ class IndividualAccountViewTestCase(ViewTestCase):
             status_code=302,
             target_status_code=200)
         self.assertTemplateUsed(response, "pages/dashboard.html")
+
+    def test_valid_account_has_targets(self):
+        self._login(self.user)
+        response: HttpResponse = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/individual_account.html")
+        account_targets: Account = response.context["account_targets"]
+        for target in account_targets:
+            self.assertTrue(isinstance(target, AccountTarget))
+            self.assertContains(response, target.transaction_type.capitalize())
+            self.assertContains(response, target.timespan.capitalize())
+            self.assertContains(response, get_completeness(target))
+            self.assertContains(response, target.currency.upper())
+            self.assertContains(response, target.amount)
+        self.assertGreater(len(account_targets), 0)
+
+    def test_valid_account_does_not_have_targets(self):
+        self._login(self.user)
+        account: PotAccount = PotAccount.objects.get_subclass(
+            user=self.user, id=5)
+        url: str = reverse(
+            "individual_account",
+            kwargs={
+                "pk": account.id,
+                "filter_type": "all"})
+        response: HttpResponse = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/individual_account.html")
+        account_targets: Account = response.context["account_targets"]
+        self.assertEqual(len(account_targets), 0)
 
     def test_invalid_get_view_redirects_when_not_logged_in(self):
         self._assert_require_login(self.url)
