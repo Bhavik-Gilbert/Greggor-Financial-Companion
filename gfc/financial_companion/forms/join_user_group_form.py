282bca8e7a762766fb0dda708495ba63b672eb90
Matthew Palmer <matthewpalmer135@gmail.com>
2023-02-16 14:34:07 +0000

    feat: Implement join user group form

---
+++ gfc/financial_companion/forms/join_user_group_form.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class JoinUserGroupForm(forms.Form):
+    """Form enabling logged in users to join user groups."""
+
+    invite_code = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'placeholder': 'Enter Group Invite Code'}))
