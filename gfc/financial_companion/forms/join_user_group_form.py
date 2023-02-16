ea1ec2d75ee8cb74d0b6b0bc617d4777cd064702
Matthew Palmer <matthewpalmer135@gmail.com>
2023-02-16 15:24:03 +0000

    style: join user group button in view groups page

--- gfc/financial_companion/forms/join_user_group_form.py
+++ gfc/financial_companion/forms/join_user_group_form.py
@@ -3,4 +3,4 @@ from django import forms
 class JoinUserGroupForm(forms.Form):
     """Form enabling logged in users to join user groups."""
 
-    invite_code = forms.CharField(label="",max_length=8, widget=forms.TextInput(attrs={'placeholder': 'Enter Group Invite Code', 'class': 'form-control'}))
+    invite_code = forms.CharField(label="",max_length=8, widget=forms.TextInput(attrs={'placeholder': 'Enter Group Invite Code', 'class': 'form-control rounded'}))
