af59615b1be122222bec00927a8694fca5049161
bhavik gilbert <bhaviklob@gmail.com>
2023-02-19 21:23:37 +0000

    style: autopep view accounts view

--- gfc/financial_companion/views/view_accounts.py
+++ gfc/financial_companion/views/view_accounts.py
@@ -8,10 +8,11 @@ from django.http import HttpRequest, HttpResponse
 def view_user_pot_accounts(request: HttpRequest) -> HttpResponse:
     bank_accounts: list[BankAccount] = BankAccount.objects.filter(
         user_id=request.user)
-        
+
     pot_accounts: list[PotAccount] = []
     for pot_account in PotAccount.objects.filter(user_id=request.user):
-        if not any(pot_account.id == bank_account.id for bank_account in bank_accounts):
+        if not any(pot_account.id ==
+                   bank_account.id for bank_account in bank_accounts):
             pot_accounts = [*pot_accounts, pot_account]
 
     return render(request, "pages/view_accounts.html",
