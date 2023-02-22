52c9b30de1f512f9a8943b717829db1b33d076a7
Reshma <reshma.dhillon@kcl.ac.uk>
2023-02-22 15:55:28 +0000

    feat: user can delete their profile

--- gfc/financial_companion/views/profile.py
+++ gfc/financial_companion/views/profile.py
@@ -2,9 +2,25 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from ..models import User
+from django.contrib import messages
 
 
 @login_required
 def profile_view(request: HttpRequest) -> HttpResponse:
     user = request.user
     return render(request, 'pages/profile.html')
+
+@login_required
+def delete_profile_view(request: HttpRequest) -> HttpResponse:
+    try:
+        user = User.objects.get(id=request.user.id)
+    except ObjectDoesNotExist:
+        return redirect('dashboard')
+    else:
+        user.delete()
+        messages.add_message(
+            request,
+            messages.WARNING,
+            "Your profile has been deleted")
+        return redirect('log_out')
