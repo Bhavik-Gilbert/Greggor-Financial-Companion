8fd5a9b74d864ae4b2aacb7ba5f79ab525a26c47
Matthew Palmer <matthewpalmer135@gmail.com>
2023-02-28 16:19:52 +0000

    feat: Implment make_owner function in user group model and implement testing

--- gfc/financial_companion/models/user_group_model.py
+++ gfc/financial_companion/models/user_group_model.py
@@ -40,3 +40,6 @@ class UserGroup(models.Model):
     def get_members(self):
         members_of_group = self.members.all()
         return ",".join([str(p) for p in members_of_group])
+    
+    def make_owner(self, user):
+        self.owner_email = user.email
