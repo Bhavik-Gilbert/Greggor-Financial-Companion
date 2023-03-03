69231f5a74b808e214dfb4f41a8af7787fab5827
bhavik gilbert <bhaviklob@gmail.com>
2023-03-03 16:35:51 +0000

    test: add assert valid or error function to helpers test base
    test: add assert valid or error function to helpers test base to either validate assertions given, or check if errors raised are expected

--- gfc/financial_companion/tests/helpers/test_helper_base.py
+++ gfc/financial_companion/tests/helpers/test_helper_base.py
@@ -1,5 +1,6 @@
 from ..test_base import BaseTestCase
-
+from typing import Any
+import warnings
 
 class HelperTestCase(BaseTestCase):
     """
@@ -7,3 +8,22 @@ class HelperTestCase(BaseTestCase):
     Call super().setUp() in the
     setUp() method of the subclass.
     """
+
+    def _assert_valid_valid_or_error(self, assert_function: callable, expected_error_message: str, warning_message: str, kwargs: dict[Any, Any]):
+        """
+        Asserts assert function is valid, or expected errors raised
+        USAGE:
+        _assert_valid_valid_or_error(
+            assert_function as function or lambda,
+            error_message as string,
+            warning_message as string,
+            kwargs as dict of values used in assert_function
+        )
+        kwargs is unwrapped for assert_function, use as direct parameter rather than kwargs
+        """
+        try:
+            assert_function(**kwargs)
+        except Exception as error:
+            warnings.warn(f"Warning: {warning_message}")
+            if not(isinstance(error, Exception) and expected_error_message in str(error).lower()):
+                raise Exception(error)
