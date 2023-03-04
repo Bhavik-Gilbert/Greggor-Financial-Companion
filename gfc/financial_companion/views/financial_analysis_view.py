253fab69ce62bb12ae372051dfc54ad9123a5fe1
Louisa Scott <louisaj3996@gmail.com>
2023-03-04 12:25:12 +0000

    feat: Add titles to graph cards

--- gfc/financial_companion/views/financial_analysis_view.py
+++ gfc/financial_companion/views/financial_analysis_view.py
@@ -27,4 +27,4 @@ def spending_summary(request: HttpRequest) -> HttpResponse:
     form = TimespanOptionsForm()
     if percentages_list == []:
         percentages_list = None
-    return render(request, "pages/spending_summary.html", {'keyset': labels, 'dataset': percentages_list, 'form': form, 'money_in': total_received, 'money_out': total_spent})
+    return render(request, "pages/spending_summary.html", {'keyset': labels, 'dataset': percentages_list, 'form': form, 'money_in': total_received, 'money_out': total_spent, 'time': str(time).capitalize()})
