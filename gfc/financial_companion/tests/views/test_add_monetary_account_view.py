077e3b0b55c7cb5544bb9904a0d1655d2bbfb38a
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2023-03-06 10:05:37 +0000

    test: regular account view tests

--- gfc/financial_companion/tests/views/test_add_monetary_account_view.py
+++ gfc/financial_companion/tests/views/test_add_monetary_account_view.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 from typing import Any
 
 from .test_view_base import ViewTestCase
-from financial_companion.forms import PotAccountForm, BankAccountForm
-from financial_companion.models import User, PotAccount, BankAccount
+from financial_companion.forms import PotAccountForm, BankAccountForm, RegularAccountForm
+from financial_companion.models import User, PotAccount, BankAccount, Account
 from financial_companion.helpers import AccountType, CurrencyType
 from decimal import Decimal
 
@@ -25,14 +25,23 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/monetary_accounts_form.html")
         form: PotAccountForm = response.context["form"]
-        self.assertTrue(isinstance(form, PotAccountForm))
+        self.assertTrue(isinstance(form, RegularAccountForm))
         form_toggle: bool = response.context["form_toggle"]
         self.assertTrue(form_toggle)
         account_type: AccountType = response.context["account_type"]
-        self.assertEqual(account_type, AccountType.POT)
+        self.assertEqual(account_type, AccountType.REGULAR)
         monetary_account_types: AccountType = response.context["monetary_account_types"]
         self.assertEqual(monetary_account_types, AccountType)
         self.assertFalse(form.is_bound)
+
+    def test_valid_post_account_type_regular(self):
+        self._login(self.user)
+        form_input: dict[str, Any] = {"account_type": AccountType.REGULAR}
+        response: HttpResponse = self.client.post(self.url, form_input)
+        account_type: AccountType = response.context["account_type"]
+        self.assertEqual(account_type, AccountType.REGULAR)
+        form: RegularAccountForm = response.context["form"]
+        self.assertTrue(isinstance(form, RegularAccountForm))
 
     def test_valid_post_account_type_pot(self):
         self._login(self.user)
@@ -51,6 +60,32 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertEqual(account_type, AccountType.BANK)
         form: PotAccountForm = response.context["form"]
         self.assertTrue(isinstance(form, BankAccountForm))
+
+    def test_valid_regular_account_form_input(self):
+        self._login(self.user)
+        regular_account_count_before: int = Account.objects.count()
+        pot_account_count_before: int = PotAccount.objects.count()
+        bank_account_count_before: int = BankAccount.objects.count()
+        form_input: dict[str, Any] = {
+            "name": "Test Regular",
+            "description": "This is a test pot",
+            "submit_type": AccountType.REGULAR
+        }
+        response: HttpResponse = self.client.post(
+            self.url, form_input, follow=True)
+        response_url: str = reverse("dashboard")
+        self.assertRedirects(
+            response,
+            response_url,
+            status_code=302,
+            target_status_code=200)
+        self.assertTemplateUsed(response, "pages/dashboard.html")
+        regular_account_count_after: int = Account.objects.count()
+        pot_account_count_after: int = PotAccount.objects.count()
+        bank_account_count_after: int = BankAccount.objects.count()
+        self.assertEqual(regular_account_count_before + 1, regular_account_count_after)
+        self.assertEqual(pot_account_count_before, pot_account_count_after)
+        self.assertEqual(bank_account_count_before, bank_account_count_after)
 
     def test_valid_pot_account_form_input(self):
         self._login(self.user)
@@ -108,6 +143,28 @@ class AddMonetaryAccountViewTestCase(ViewTestCase):
         self.assertEqual(
             bank_account_count_before + 1,
             bank_account_count_after)
+        
+    def test_invalid_regular_account_form_input(self):
+        self._login(self.user)
+        regualr_account_count_before: int = Account.objects.count()
+        pot_account_count_before: int = PotAccount.objects.count()
+        bank_account_count_before: int = BankAccount.objects.count()
+        form_input: dict[str, Any] = {
+            "name": "Test regular",
+            "description": "This is a test account",
+            "currency": CurrencyType.GBP,
+            "submit_type": AccountType.POT
+        }
+        response: HttpResponse = self.client.post(
+            self.url, form_input, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/monetary_accounts_form.html")
+        regualr_account_count_after: int = Account.objects.count()
+        pot_account_count_after: int = PotAccount.objects.count()
+        bank_account_count_after: int = BankAccount.objects.count()
+        self.assertEqual(regualr_account_count_before, regualr_account_count_after)
+        self.assertEqual(pot_account_count_before, pot_account_count_after)
+        self.assertEqual(bank_account_count_before, bank_account_count_after)
 
     def test_invalid_pot_account_form_input(self):
         self._login(self.user)
