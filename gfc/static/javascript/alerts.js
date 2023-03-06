0ebdf3f52a017305c31e39679ca41d08b51d21ff
Reshma <reshma.dhillon@kcl.ac.uk>
2023-03-06 16:56:34 +0000

    Refactor: move script for autococomplete to a separate function

--- gfc/static/javascript/alerts.js
+++ gfc/static/javascript/alerts.js
@@ -1,3 +1,9 @@
 function showAlert(name) {
   confirm("Are you sure you want to delete this " + name + "?");
 }
+
+function autocomplete(id){
+  $(document).ready(function() {
+    $(id).select2();
+  });
+}
