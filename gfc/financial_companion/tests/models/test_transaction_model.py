67f2068430d29d8b05293f0cf5274b0ee610642a
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2023-03-07 15:18:44 +0000

    test: sender account balance increases and decreases on edit transaction

--- gfc/financial_companion/tests/models/test_transaction_model.py
+++ gfc/financial_companion/tests/models/test_transaction_model.py
@@ -16,6 +16,13 @@ class TransactionModelTestCase(ModelTestCase):
         self.user: User = User.objects.get(id=1)
         self.sender_account: PotAccount = PotAccount.objects.get(id=5)
         self.receiver_account: PotAccount = PotAccount.objects.get(id=6)
+        self.transaction_model: Transaction = Transaction()
+        self.transaction_model.title= "New laptop"
+        self.transaction_model.description= "Bought new laptop"
+        self.transaction_model.amount = round(Decimal(2099.99),2)
+        self.transaction_model.currency = CurrencyType.GBP
+        self.transaction_model.sender_account = self.sender_account
+        self.transaction_model.receiver_account = self.receiver_account
 
     def test_valid_transaction(self):
         self._assert_model_is_valid()
@@ -28,31 +35,45 @@ class TransactionModelTestCase(ModelTestCase):
         self.test_model.time_of_transaction = ""
         self._assert_model_is_valid()
 
-    def test_sender_account_balance_decreases(self):
+    def test_sender_account_balance_decreases_on_create_transaction(self):
         self.sender_balance_before_transaction = self.sender_account.balance
-        transaction_model: Transaction = Transaction.objects.create(
-            title= "New laptop",
-            description= "Bought new laptop",
-            amount = round(Decimal(2099.99),2),
-            currency = CurrencyType.GBP,
-            sender_account = self.sender_account,
-            receiver_account = self.receiver_account
-        )
+        self.transaction_model.save()
         self.sender_account.refresh_from_db()
         self.sender_balance_after_transaction = self.sender_account.balance
-        self.assertEqual(Decimal(self.sender_balance_before_transaction - transaction_model.amount), self.sender_balance_after_transaction)
+        self.assertEqual(Decimal(self.sender_balance_before_transaction - self.transaction_model.amount), self.sender_balance_after_transaction)
 
-    def test_receiver_account_balance_increases(self):
+    def test_receiver_account_balance_increases_on_create_transaction(self):
         self.receiver_balance_before_transaction = self.receiver_account.balance
-        transaction_model2: Transaction = Transaction.objects.create(
-            title= "New case",
-            description= "Bought new case",
-            amount = round(Decimal(2099.99),2),
-            currency = CurrencyType.GBP,
-            sender_account = self.sender_account,
-            receiver_account = self.receiver_account
-        )
+        self.transaction_model.save()
         self.receiver_account.refresh_from_db()
         self.receiver_balance_after_transaction = self.receiver_account.balance
-        self.assertEqual(Decimal(self.receiver_balance_before_transaction + transaction_model2.amount), self.receiver_balance_after_transaction)
+        self.assertEqual(Decimal(self.receiver_balance_before_transaction + self.transaction_model.amount), self.receiver_balance_after_transaction)
+    
+    def test_sender_account_balance_increases_on_edit_transaction(self):
+        self.transaction_model.save()
+        self.sender_account.refresh_from_db()
+        self.sender_balance_before_transaction = self.sender_account.balance
+        self.old_transaction_amount = self.transaction_model.amount
+        self.transaction_model.amount = round(Decimal(1800.00),2)
+        self.transaction_model.save()
+        self.sender_account.refresh_from_db()
+        self.new_transaction_amount = self.transaction_model.amount
+        self.sender_balance_after_transaction = self.sender_account.balance
+        self.transaction_amount_diff = round(Decimal(self.old_transaction_amount - self.new_transaction_amount),2)
+        self.assertEqual(Decimal(self.sender_balance_before_transaction + self.transaction_amount_diff), self.sender_balance_after_transaction)
+
+    def test_sender_account_balance_decreases_on_edit_transaction(self):
+        self.transaction_model.save()
+        self.sender_account.refresh_from_db()
+        self.sender_balance_before_transaction = self.sender_account.balance
+        self.old_transaction_amount = self.transaction_model.amount
+        self.transaction_model.amount = round(Decimal(2800.00),2)
+        self.transaction_model.save()
+        self.sender_account.refresh_from_db()
+        self.new_transaction_amount = self.transaction_model.amount
+        self.sender_balance_after_transaction = self.sender_account.balance
+        self.transaction_ammount_diff = round(Decimal(self.old_transaction_amount - self.new_transaction_amount),2)
+        self.assertEqual(Decimal(self.sender_balance_before_transaction + self.transaction_amount_diff), self.sender_balance_after_transaction)
+    
+
     
