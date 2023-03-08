dc5711e437c0cc0ee884c2ef928a8ea7d74fcbdc
bhavik gilbert <bhaviklob@gmail.com>
2023-03-08 10:30:20 +0000

    fix: implement django form for quiz

---
+++ gfc/financial_companion/forms/quiz_form.py
@@ -0,0 +1,41 @@
+from django import forms
+from financial_companion.models import QuizQuestion, QuizSet, QuizScore
+from typing import Any
+
+
+class QuizQuestionForm(forms.Form):
+    """Form to answer quiz questions"""
+
+    def __init__(self, user, quiz_set, *args, **kwargs):
+        self.quiz_set: QuizSet = quiz_set
+        self.user: QuizSet = user
+        super(QuizQuestionForm, self).__init__(*args, **kwargs)
+
+        if self.quiz_set is not None:
+            for quiz_question in self.quiz_set.questions.all():
+                self.fields[f"{quiz_question.id}"] = forms.ChoiceField(
+                    choices=[(answer, answer) for answer in quiz_question.get_potential_answers()],
+                    widget=forms.RadioSelect,
+                    required=True,
+                    label=quiz_question.question
+                )
+        
+    def save(self) -> QuizScore:
+        if not self.is_valid():
+            return None
+        total_questions: int = self.quiz_set.questions.count()
+        correct_answers: int = 0
+
+        for form_question in self.cleaned_data.keys():
+            quiz_question: QuizQuestion = self.quiz_set.questions.all().get(id=form_question)
+            if quiz_question.is_answer(self.cleaned_data[form_question]):
+                correct_answers += 1
+        
+        quiz_score: QuizScore = QuizScore.objects.create(
+                user=self.user,
+                total_questions=total_questions,
+                correct_questions=correct_answers,
+                quiz_set=self.quiz_set
+            )
+
+        return quiz_score
---
+++ gfc/financial_companion/tests/forms/test_quiz_question_form.py
@@ -0,0 +1,57 @@
+"""Unit tests of the log in form."""
+from django import forms
+from .test_form_base import FormTestCase
+from financial_companion.forms import QuizQuestionForm
+from financial_companion.models import User, QuizQuestion, QuizSet, QuizScore
+from typing import Any
+
+
+class QuizQuestionFormTestCase(FormTestCase):
+    """Unit tests of quiz_question form."""
+
+    def setUp(self):
+        self.user: User = User.objects.get(username='@johndoe')
+        self.quiz_set: QuizSet = QuizSet.objects.get(id=1)
+        self.form_input: dict[str, Any] = {"quiz_submit": ""}
+        for question in self.quiz_set.questions.all():
+            self.form_input[str(question.id)] = question.potential_answer_1
+
+    def test_valid_form_contains_required_fields(self):
+        form: QuizQuestionForm = QuizQuestionForm(self.user, self.quiz_set)
+        self._assert_form_has_necessary_fields(
+            form,
+            *[str(question.id) for question in self.quiz_set.questions.all()]
+        )
+        for field in form.fields:
+            self.assertTrue(isinstance(form.fields[field].widget, forms.RadioSelect))
+
+    def test_valid_form_accepts_valid_input(self):
+        form: QuizQuestionForm = QuizQuestionForm(self.user, self.quiz_set, data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form_rejects_blank_input(self):
+        for field in self.form_input:
+            if field == "quiz_submit":
+                continue
+            form_input: dict[str, Any] = self.form_input.copy()
+            form_input.pop(field, None)
+            form: QuizQuestionForm = QuizQuestionForm(self.user, self.quiz_set, data=form_input)
+            self.assertFalse(form.is_valid())
+    
+    def test_valid_save_quiz_score_created_with_correct_input(self):
+        form: QuizQuestionForm = QuizQuestionForm(self.user, self.quiz_set, data=self.form_input)
+        quiz_score_before: int = QuizScore.objects.count()
+        quiz_set: QuizScore = form.save()
+        quiz_score_after: int = QuizScore.objects.count()
+        self.assertEqual(quiz_score_before + 1, quiz_score_after)
+        self.assertTrue(isinstance(quiz_set, QuizScore))
+    
+    def test_invalid_save_quiz_score_not_created_with_incorrect_input(self):
+        form_input: dict[str, Any] = self.form_input
+        form_input.pop(str(self.quiz_set.questions.all().first().id))
+        form: QuizQuestionForm = QuizQuestionForm(self.user, self.quiz_set, data=form_input)
+        quiz_score_before: int = QuizScore.objects.count()
+        quiz_set: QuizScore = form.save()
+        quiz_score_after: int = QuizScore.objects.count()
+        self.assertEqual(quiz_score_before, quiz_score_after)
+        self.assertTrue(quiz_set is None)
