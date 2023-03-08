ddcc80b7253c894f913902e378253664be7109fb
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2023-03-08 18:55:14 +0000

    test: create tests for category view with search redirect

--- gfc/financial_companion/tests/views/test_category_list_view.py
+++ gfc/financial_companion/tests/views/test_category_list_view.py
@@ -11,69 +11,69 @@ class CategoryListViewCase(ViewTestCase):
         self.url = reverse('categories_list', kwargs={'search_name': "all"})
         self.user = User.objects.get(username='@johndoe')
 
-    # def test_log_in_url(self):
-    #     self.assertEqual(self.url, '/categories/all/')
+    def test_log_in_url(self):
+        self.assertEqual(self.url, '/categories/all/')
 
-    # def test_post_when_search_is_empty(self):
-    #     self._login(self.user)
-    #     response = self.client.post(self.url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'pages/category_list.html')
-    #     messages_list = list(response.context['messages'])
-    #     self.assertEqual(len(messages_list), 1)
-    #     self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
-    #     self.assertContains(response, "Food")
-    #     self.assertContains(response, "Eating out expenses")
-    #     self.assertContains(response, "Travel")
-    #     self.assertContains(response, "Going to and from uni")
-    #     self.assertContains(response, 'Entertainment')
-    #     self.assertContains(response, 'Going out and having fun')
+    def test_post_when_search_is_empty(self):
+        self._login(self.user)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/category_list.html')
+        messages_list = list(response.context['messages'])
+        self.assertEqual(len(messages_list), 1)
+        self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
+        self.assertContains(response, "Food")
+        self.assertContains(response, "Eating out expenses")
+        self.assertContains(response, "Travel")
+        self.assertContains(response, "Going to and from uni")
+        self.assertContains(response, 'Entertainment')
+        self.assertContains(response, 'Going out and having fun')
 
-    # def test_post_when_full_category_name_is_applied(self):
-    #     self._login(self.user)
-    #     self.url = reverse('categories_list', kwargs={'search_name': "Food"})
-    #     response = self.client.post(self.url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'pages/category_list.html')
-    #     messages_list = list(response.context['messages'])
-    #     self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
-    #     self.assertEqual(len(messages_list), 1)
-    #     self.assertContains(response, 'Food')
-    #     self.assertContains(response, "Eating out expenses")
+    def test_post_when_full_category_name_is_applied(self):
+        self._login(self.user)
+        self.url = reverse('categories_list', kwargs={'search_name': "Food"})
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/category_list.html')
+        messages_list = list(response.context['messages'])
+        self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
+        self.assertEqual(len(messages_list), 1)
+        self.assertContains(response, 'Food')
+        self.assertContains(response, "Eating out expenses")
 
-    # def test_post_when_partial_category_name_is_applied(self):
-    #     self._login(self.user)
-    #     self.url = reverse('categories_list', kwargs={'search_name': "e"})
-    #     response = self.client.post(self.url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'pages/category_list.html')
-    #     messages_list = list(response.context['messages'])
-    #     self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
-    #     self.assertEqual(len(messages_list), 1)
-    #     self.assertContains(response, "Travel")
-    #     self.assertContains(response, "Going to and from uni")
-    #     self.assertContains(response, 'Entertainment')
-    #     self.assertContains(response, 'Going out and having fun')
+    def test_post_when_partial_category_name_is_applied(self):
+        self._login(self.user)
+        self.url = reverse('categories_list', kwargs={'search_name': "e"})
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/category_list.html')
+        messages_list = list(response.context['messages'])
+        self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
+        self.assertEqual(len(messages_list), 1)
+        self.assertContains(response, "Travel")
+        self.assertContains(response, "Going to and from uni")
+        self.assertContains(response, 'Entertainment')
+        self.assertContains(response, 'Going out and having fun')
 
-    # def test_post_when_incorrect_category_name_is_applied(self):
-    #     self._login(self.user)
-    #     self.url = reverse(
-    #         'categories_list', kwargs={
-    #             'search_name': "Shopping"})
-    #     response = self.client.post(self.url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'pages/category_list.html')
-    #     messages_list = list(response.context['messages'])
-    #     self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
-    #     self.assertEqual(len(messages_list), 1)
-    #     # "Food" is found in warning messages
-    #     self.assertContains(response, "Food")
-    #     self.assertNotContains(response, "Eating out expenses")
-    #     self.assertNotContains(response, "Travel")
-    #     self.assertNotContains(response, "Going to and from uni")
-    #     self.assertNotContains(response, 'Entertainment')
-    #     self.assertNotContains(response, 'Going out and having fun')
-    #     self.assertContains(response, "You have no categories yet")
+    def test_post_when_incorrect_category_name_is_applied(self):
+        self._login(self.user)
+        self.url = reverse(
+            'categories_list', kwargs={
+                'search_name': "Shopping"})
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/category_list.html')
+        messages_list = list(response.context['messages'])
+        self.assertTrue('Targets exceeded: ' in str(messages_list[0]))
+        self.assertEqual(len(messages_list), 1)
+        # "Food" is found in warning messages
+        self.assertContains(response, "Food")
+        self.assertNotContains(response, "Eating out expenses")
+        self.assertNotContains(response, "Travel")
+        self.assertNotContains(response, "Going to and from uni")
+        self.assertNotContains(response, 'Entertainment')
+        self.assertNotContains(response, 'Going out and having fun')
+        self.assertContains(response, "You have no categories yet")
     
     def test_view_redirects_when_search_button_pressed_for_valid_search_name(self):
         self.form_data = {
@@ -93,5 +93,5 @@ class CategoryListViewCase(ViewTestCase):
         response = self.client.post(self.url, self.form_data)
         self.assertEqual(response.status_code, 302)
 
-    # def test_get_view_redirects_when_not_logged_in(self):
-    #     self._assert_require_login(self.url)
+    def test_get_view_redirects_when_not_logged_in(self):
+        self._assert_require_login(self.url)
