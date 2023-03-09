b81ea14d6a8b45c8e5a2d79501710cb34e06db35
Louisa Scott <louisaj3996@gmail.com>
2023-03-09 18:32:08 +0000

    feat: Add recurring transaction page link to nav bar via transaction dropdown

--- gfc/financial_companion/views/individual_recurring_transaction.py
+++ gfc/financial_companion/views/individual_recurring_transaction.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from django.conf import settings
 from ..models import RecurringTransaction
+from financial_companion.helpers import paginate
 
 
 @login_required
@@ -14,5 +15,6 @@ def individual_recurring_transaction_view(request: HttpRequest, pk: int) -> Http
     except RecurringTransaction.DoesNotExist:
         return redirect("dashboard")
     else:
+        transactionslist= paginate(request.GET.get('page', 1), transaction.transactions.all())
         return render(request, "pages/individual_recurring_transaction.html",
-                      {"transaction": transaction, "transactionslist": list(transaction.transactions.all())})
+                      {"transaction": transaction, "transactionslist": transactionslist})
