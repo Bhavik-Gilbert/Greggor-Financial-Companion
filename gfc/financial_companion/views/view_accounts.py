b18c125308a06d3ed4eb9003a3eb11ab0f4c1989
Splagon <misha.higham@hotmail.co.uk>
2023-03-11 14:57:36 +0000

    fix: view user accounts properly seperates account types and no longer repeats accounts

--- gfc/financial_companion/views/view_accounts.py
+++ gfc/financial_companion/views/view_accounts.py
@@ -7,24 +7,27 @@ from django.http import HttpRequest, HttpResponse
 
 @login_required
 def view_user_pot_accounts(request: HttpRequest) -> HttpResponse:
-    all_user_monetary_accounts = PotAccount.objects.filter(
+    all_user_accounts = Account.objects.filter(
         user_id=request.user)
-    bank_accounts: list[BankAccount] = BankAccount.objects.filter(
+    all_user_pot_accounts: list[PotAccount] = PotAccount.objects.filter(
         user_id=request.user)
-    pot_accounts: list[PotAccount] = all_user_monetary_accounts.exclude(
-        pk__in=bank_accounts)
-    all_user_non_monetary_accounts = Account.objects.filter(user=request.user)
-    accounts: list[Account] = all_user_non_monetary_accounts.exclude(
-        pk__in=pot_accounts)
+
+    user_bank_accounts_only: list[BankAccount] = BankAccount.objects.filter(
+        user_id=request.user)
+    user_pot_accounts_only = all_user_pot_accounts.exclude(
+        pk__in=user_bank_accounts_only)
+    user_accounts_only = all_user_accounts.exclude(
+        pk__in=all_user_pot_accounts)
+    
 
     targetsForMessages = request.user.get_all_account_targets(
-        all_user_monetary_accounts)
+        all_user_accounts)
     request = get_warning_messages_for_targets(
         request, False, targetsForMessages)
 
     return render(request, "pages/view_accounts.html",
                   {
-                      'accounts': accounts,
-                      'pot_accounts': pot_accounts,
-                      'bank_accounts': bank_accounts
+                      'accounts': user_accounts_only,
+                      'pot_accounts': user_pot_accounts_only,
+                      'bank_accounts': user_bank_accounts_only
                   })
