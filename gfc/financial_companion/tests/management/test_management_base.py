8c5fba69dd08fe3f802f6a8b85ca6beb6d0b682f
bhavik gilbert <bhaviklob@gmail.com>
2023-03-15 18:26:30 +0000

    test: test seeder and unseeder

--- gfc/financial_companion/tests/management/test_management_base.py
+++ gfc/financial_companion/tests/management/test_management_base.py
@@ -1,13 +1,32 @@
-from django.core.exceptions import ValidationError
-from django.db.models import Model
-
-from ..test_base import BaseTestCase
+from django.test import TestCase
+import sys
 
 
-class ManagementTestCase(BaseTestCase):
+class NullOutput(object):
+    """
+    Redirect all output into nowhere
+    """
+
+    def write(self, *args) -> None:
+        pass
+
+    def flush(self, *args) -> None:
+        pass
+
+class ManagementTestCase(TestCase):
     """
     Base class for testing management commands.
     Call super().setUp() and set
     self.test_model_base to the model to be tested
     in the setUp() method of the subclass.
     """
+
+    def setUp(self):
+        super().setUp()
+        # redirect stdout into nothing
+        self.sysout = sys.stdout
+        sys.stdout = NullOutput()
+
+    def tearDown(self):
+        # restore stdout
+        sys.stdout = self.sysout
