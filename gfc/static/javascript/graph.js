117d6cd242303f917746aee1ad750b2fa7b3c25b
bhavik gilbert <bhaviklob@gmail.com>
2023-03-17 00:55:26 +0000

    refactor: move graph javascript into graph.js

---
+++ gfc/static/javascript/graph.js
@@ -0,0 +1,58 @@
+function setChart(chartDataset, chartLabels, yAxisLabel="", xAxisLabel="", chartType="line", yBeginsAtZero=false, xBeginsAtZero=false) {
+    const ctx = document.getElementById("chart"+ chartName);
+
+    if (ctx.attributes.length > 1) { // if there is a chart in the canvas.
+          Chart.getChart(ctx).destroy(); // clear the canvas
+    }
+
+    Chart.defaults.color = '{{primary}} !IMPORTANT';
+    if (chartType == "line"| chartType == "bar") {
+      setLineChart(ctx, chartDataset, chartLabels, yAxisLabel, xAxisLabel, yBeginsAtZero, xBeginsAtZero)
+    }
+    else if (chartType == "pie" || chartType == "doughnut") {
+        setRoundChart(ctx, chartType, chartLabels, yAxisLabel, chartDataset)
+    }
+}
+
+function setLineChart(ctx, chartDataset, chartLabels, yAxisLabel="", xAxisLabel="", yBeginsAtZero=false, xBeginsAtZero=false) {
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: chartLabels,
+            datasets: chartDataset
+        },
+        options: {
+            scales: {
+            y: {
+                title: {
+                    display: true,
+                    text: yAxisLabel,
+                    },
+                beginAtZero: yBeginsAtZero
+            },
+            x: {
+                title: {
+                    display: true,
+                    text: xAxisLabel,
+                    },
+                beginAtZero: xBeginsAtZero
+            }
+            },
+            aspectRatio: 1.7
+        }
+    });
+}
+
+function setRoundChart(ctx, chartType, chartLabels, dataLabels, chartDataset) {
+    new Chart(ctx, {
+        type: chartType,
+        data: {
+            labels: chartLabels,
+            datasets: [{
+            label: dataLabels,
+            data: chartDataset,
+            borderWidth: 1
+            }]
+        },
+    });
+}
