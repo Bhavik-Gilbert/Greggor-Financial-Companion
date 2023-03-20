35792821a7581dc444f19d8759ddace56db9f761
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2023-03-20 16:24:43 +0000

    feat: remove double call for quiz_scores.delete

--- gfc/financial_companion/management/commands/unseed.py
+++ gfc/financial_companion/management/commands/unseed.py
@@ -28,11 +28,8 @@ class Command(BaseCommand):
         print(f"UNSEEDING IN PROGRESS", end='\r')
 
         users: QuerySet[User] = User.objects.filter(email__endswith='@gfc.org')
-        quiz_scores: QuerySet[QuizScore] = QuizScore.objects.filter(
-            user__in=user)
 
         users.delete()
-        quiz_scores.delete()
         Schedule.objects.all().delete()
 
         QuizSet.objects.filter(seeded=True).delete()
