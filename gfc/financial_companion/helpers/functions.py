e61ed10154ba9c9ea983dcbae218c04cc8aa0e23
bhavik gilbert <bhaviklob@gmail.com>
2023-03-21 18:44:23 +0000

    refactor: helpers cleanup

--- gfc/financial_companion/helpers/functions.py
+++ gfc/financial_companion/helpers/functions.py
@@ -14,6 +14,7 @@ import random
 import string
 from typing import Any
 from django.http import HttpRequest
+from django.db.models import QuerySet
 
 
 def get_currency_symbol(currency_code: str) -> str:
@@ -53,7 +54,8 @@ def random_filename(filename: str) -> str:
     return '{}.{}'.format(''.join(filename_strings_to_add), file_extension)
 
 
-def calculate_percentages(spent_per_category: dict(), total: float) -> dict:
+def calculate_percentages(
+        spent_per_category: dict[str, float], total: float) -> dict[str, float]:
     """calculate percentage of expenditure taken up by each category"""
     for key, value in spent_per_category.items():
         percentage: float = float((value / total) * 100)
@@ -116,14 +118,15 @@ def get_projections_balances(accounts, max_timescale_in_months: int = max(
                 account.interest_rate)}
         balances: list[float] = []
         current_balance: float = float(account.balance)
-        i = 1
-        while i <= max_timescale_in_months:
+        timescale_counter_in_months = 1
+        while timescale_counter_in_months <= max_timescale_in_months:
             temp_balance_total: float = current_balance * \
-                ((1 + (interest_rate / 365))**get_number_of_days_in_prev_month(i))
+                ((1 + (interest_rate / 365)) **
+                 get_number_of_days_in_prev_month(timescale_counter_in_months))
             if temp_balance_total >= 0:
                 current_balance = temp_balance_total
             balances.append(current_balance)
-            i += 1
+            timescale_counter_in_months += 1
         account_data.update({"balances": balances})
         account_dictionary.update({account.id: account_data})
 
@@ -134,22 +137,24 @@ def get_short_month_names_for_timescale(
         max_timescale_in_months: int = max(get_projection_timescale_options().keys())) -> list[str]:
     """returns month names in the given time scale"""
     current_date: datetime = datetime.today()
-    i = 1
+    timescale_counter_in_months = 1
     dates: list[str] = []
-    while i <= max_timescale_in_months:
-        next_date: datetime = current_date + relativedelta(months=i)
+    while timescale_counter_in_months <= max_timescale_in_months:
+        next_date: datetime = current_date + \
+            relativedelta(months=timescale_counter_in_months)
         dates.append(str(next_date.strftime("%b").capitalize() +
                      " " + next_date.strftime("%y").capitalize()))
-        i += 1
+        timescale_counter_in_months += 1
 
     return dates
 
 
 def get_number_of_days_in_prev_month(offset_inMonths: int = 0) -> int:
     """calculate the number of days in the previous month"""
-    date: datetime = datetime.today() + relativedelta(months=offset_inMonths)
+    last_month_start_date: datetime = datetime.today(
+    ) + relativedelta(months=offset_inMonths)
     no_of_days_in_prev_month: int = (
-        date.replace(
+        last_month_start_date.replace(
             day=1) -
         timedelta(
             days=1)).day
@@ -159,28 +164,28 @@ def get_number_of_days_in_prev_month(offset_inMonths: int = 0) -> int:
 
 def get_data_for_account_projection(user) -> dict[str, Any]:
     """return a dicationary contining all information for savings projections for all of the users accounts"""
-    accounts = fcmodels.BankAccount.objects.filter(
+    accounts: QuerySet[fcmodels.BankAccount] = fcmodels.BankAccount.objects.filter(
         user_id=user, interest_rate__gt=0)
-    mainCurrency: str = "GBP"
+    main_currency: CurrencyType = CurrencyType.GBP
     if (accounts):
-        mainCurrency: str = accounts[0].currency
+        main_currency: str = accounts[0].currency
     conversions: dict[str, float] = get_conversions_for_accounts(
-        accounts, mainCurrency)
+        accounts, main_currency)
 
     timescale_dict: dict[int, str] = get_projection_timescale_options()
     timescales_strings: list[str] = get_short_month_names_for_timescale()
 
-    accountsDictionary: dict[str, list[float]
-                             ] = get_projections_balances(accounts)
+    accounts_dictionary: dict[str, list[float]
+                              ] = get_projections_balances(accounts)
 
     return {
         'bank_accounts': {acc.id: acc.name for acc in accounts},
-        'bank_account_infos': dumps(accountsDictionary),
+        'bank_account_infos': dumps(accounts_dictionary),
         'timescale_dict': timescale_dict,
         'timescales_strings': timescales_strings,
         'conversion_to_main_currency_JSON': dumps(conversions),
         'conversion_to_main_currency': conversions,
-        'main_currency': mainCurrency
+        'main_currency': main_currency
     }
 
 
@@ -210,21 +215,22 @@ def get_sorted_members_based_on_completed_targets(
 
 def get_warning_messages_for_targets(
         request: HttpRequest, show_numbers_for_multiples: bool = True, targets: list = None) -> HttpRequest:
-    """Return a httprequest containing messages for nearly exceeded, exceeded and completed targets"""
+    """Return a http request containing messages for nearly exceeded, exceeded and completed targets"""
     if not targets:
-        targets: list = request.user.get_all_targets()
-    completed_targets: list = request.user.get_completed_targets(targets)
-    nearly_completed_targets: list = request.user.get_nearly_completed_targets(
+        targets: list[fcmodels.AbstractTarget] = request.user.get_all_targets()
+    completed_targets: list[fcmodels.AbstractTarget] = request.user.get_completed_targets(
+        targets)
+    nearly_completed_targets: list[fcmodels.AbstractTarget] = request.user.get_nearly_completed_targets(
         targets)
 
     sorted_targets_dict: dict[str, dict[str, list[fcmodels.AbstractTarget]]] = {
-        'completed': {}, 'nearlyExceeded': {}, 'exceeded': {}}
+        'completed': {}, 'nearly_exceeded': {}, 'exceeded': {}}
     for target in targets:
         dictionary_to_add: dict[str, list[fcmodels.AbstractTarget]] = None
         if target.target_type == 'income' and target in completed_targets:
             dictionary_to_add = sorted_targets_dict['completed']
         elif target.target_type == 'expense' and target in nearly_completed_targets:
-            dictionary_to_add = sorted_targets_dict['nearlyExceeded']
+            dictionary_to_add = sorted_targets_dict['nearly_exceeded']
         elif target.target_type == 'expense' and target in completed_targets:
             dictionary_to_add = sorted_targets_dict['exceeded']
 
@@ -266,13 +272,13 @@ def get_warning_messages_for_targets(
                 list(sorted_targets_dict['completed'].values()))
         )
 
-    if sorted_targets_dict['nearlyExceeded']:
+    if sorted_targets_dict['nearly_exceeded']:
         messages.add_message(
             request,
             messages.WARNING,
             'Targets nearly exceeded: ' +
             convert_list_to_string(
-                list(sorted_targets_dict['nearlyExceeded'].values()))
+                list(sorted_targets_dict['nearly_exceeded'].values()))
         )
 
     if sorted_targets_dict['exceeded']:
@@ -288,7 +294,7 @@ def get_warning_messages_for_targets(
 
 
 def convert_list_to_string(list_in: list[Any]) -> str:
-    """returns an inputed list as a string"""
+    """Returns an input list as a string"""
     output: str = ""
     list_length: int = len(list_in)
     if list_length >= 1:
