ebcd56e8e3cf56aace368da2f32d5e7609ff5b61
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2023-03-22 14:24:18 +0000

    style: format code using autopep

--- gfc/financial_companion/models/user_model.py
+++ gfc/financial_companion/models/user_model.py
@@ -176,5 +176,5 @@ class User(AbstractUser):
         score: float = 0
         score += self.get_number_of_completed_spending_targets() + -(self.get_number_of_completed_saving_targets()) + \
             -(0.5 * self.get_number_of_nearly_completed_saving_targets()
-             ) + (0.5 * self.get_number_of_nearly_completed_spending_targets())
+              ) + (0.5 * self.get_number_of_nearly_completed_spending_targets())
         return score
